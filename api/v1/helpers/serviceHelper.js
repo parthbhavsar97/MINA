8187525654d58b4035b64720144728eae6e97a21
Parth <parth.bhavsar@tristatetechnology.co.in>
2020-11-25 19:35:42 +0530

    query changes for getServices API

--- api/v1/helpers/serviceHelper.js
+++ api/v1/helpers/serviceHelper.js
@@ -7,11 +7,14 @@ class ServiceHelper {
 
   async getServices(body) {
     try {
-        let condition = ` is_deleted = 0 ORDER BY service_id ASC`
-        let services = await db.select('service', '*', condition)
-        return services
+      let select = `*,
+        (SELECT COUNT(chapter_id) AS total_chapters FROM chapter where chapter.service_id = service.service_id AND chapter.is_deleted = 0) as total_chapter,
+        (SELECT COUNT(session_id) AS total_session FROM session where session.service_id = service.service_id AND session.is_deleted = 0) as total_session`
+      let condition = ` is_deleted = 0 ORDER BY service_id ASC`
+      let services = await db.select('service', select, condition)
+      return services
     } catch (error) {
-        throw error
+      throw error
     }
   }
 
